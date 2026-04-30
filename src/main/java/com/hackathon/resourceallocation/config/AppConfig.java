6c9ed4fd26e03faa90f5159ebcc9d58cc283b839
Abhay Raj <abhayraj620285@gmail.com>
2026-04-30 20:20:26 +0530

    updated

--- src/main/java/com/hackathon/resourceallocation/config/AppConfig.java
+++ src/main/java/com/hackathon/resourceallocation/config/AppConfig.java
@@ -19,7 +19,7 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+              //  .allowedOrigins("http://localhost:3000", "http://localhost:5173")
                 .allowedOrigins("https://relief-fronted-4qvq.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
